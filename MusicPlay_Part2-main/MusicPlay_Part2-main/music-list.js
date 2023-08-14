990f150e57d0b4211e16a46b14e1a4e1ac3fd15e
ngochinh66 <142208452+ngochinh66@users.noreply.github.com>
2023-08-14 15:19:06 +0700

    update

---
+++ MusicPlay_Part2-main/MusicPlay_Part2-main/music-list.js
@@ -0,0 +1,60 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "henemkiepsau",
+    artist: "Hồng Thanh,Mie",
+    img: "Dam-Cuoi-Nha",
+    src: "henemkiepsau"
+  },
+ 
+  {
+    name: "mbd",
+    artist: "Hoàng Thùy Linh",
+    img: "See-Tinh",
+    src: "mbd"
+  },
+  {
+    name: "may",
+    artist: "Huy Vạc",
+    img: "ChiMuonBenEmLucNay",
+    src: "may"
+  },
+  {
+    name: "em1",
+    artist: "Jack",
+    img: "Hong-Nhan-Jack",
+    src: "em1"
+  },
+  {
+    name: "hoi uc ver2",
+    artist: "YLinh,Xám",
+    img: "Chi-Muon-Ben-Em-That-Gan",
+    src: "Hồi Ức V2 - Thái Hoàng"
+  },
+  {
+    name: "Em Là Nhất",
+    artist: "ssss",
+    img: "Em-La-Nhat",
+    src: "Em-La-Nhat"
+  },
+  {
+    name: "thai hoang ",
+    artist: "Lilk",
+    img: "thaihaong",
+    src: "TH - Gag"
+  },
+  {
+    name: "ngaymai",
+    artist: "Lilk",
+    img: "ngaymai",
+    src: "ngaymai"
+  },
+];
