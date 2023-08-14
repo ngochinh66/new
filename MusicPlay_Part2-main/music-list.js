cee95d6221443dfdad54757780c347ee9b4eae66
ngochinh66 <142208452+ngochinh66@users.noreply.github.com>
2023-08-14 14:45:54 +0700

    init

---
+++ MusicPlay_Part2-main/music-list.js
@@ -0,0 +1,60 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "henemkiepsau",
+    artist: "Hồng Thanh,Mie",
+    img: "Dam-Cuoi-Nha",
+    src: "henemkiepsau"
+  },
+ 
+  {
+    name: "See Tình",
+    artist: "Hoàng Thùy Linh",
+    img: "See-Tinh",
+    src: "See-Tinh"
+  },
+  {
+    name: "Chỉ Muốn Bên Em Lúc Này",
+    artist: "Huy Vạc",
+    img: "ChiMuonBenEmLucNay",
+    src: "ChiMuonBenEmLucNay"
+  },
+  {
+    name: "Hồng Nhan",
+    artist: "Jack",
+    img: "Hong-Nhan-Jack",
+    src: "Hong-Nhan-Jack"
+  },
+  {
+    name: "Chỉ Muốn Bên Em Thật Gần",
+    artist: "YLinh,Xám",
+    img: "Chi-Muon-Ben-Em-That-Gan",
+    src: "Chi-Muon-Ben-Em-That-Gan"
+  },
+  {
+    name: "Em Là Nhất",
+    artist: "ssss",
+    img: "Em-La-Nhat",
+    src: "Em-La-Nhat"
+  },
+  {
+    name: "thai hoang ",
+    artist: "Lilk",
+    img: "thaihaong",
+    src: "TH - Gag"
+  },
+  {
+    name: "em",
+    artist: "Lilk",
+    img: "ngaymai",
+    src: "ngaymai"
+  },
+];
